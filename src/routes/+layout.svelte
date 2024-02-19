5f3e74aba27e41c7e14c94405049f42f67965e66
zenaxo <sjolander48@live.se>
2024-02-19 08:41:28 +0100

    favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import appleTouchIcon from '$lib/icons/apple-touch-icon.png';
+	import favIconLarge from '$lib/icons/favicon-32x32.png';
+	import favIconSmall from '$lib/icons/favicon-16x16.png';
+	import manifest from '../site.webmanifest';
+	import maskIcon from '$lib/icons/safari-pinned-tab.svg';
+
 	import '../app.css';
 	import Nav from '../components/Nav/Nav.svelte';
 </script>
 
 <svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="src/image/png" sizes="32x32" href={favIconLarge} />
+	<link rel="icon" type="src/image/png" sizes="16x16" href={favIconSmall} />
+	<link rel="manifest" href={manifest} />
+	<link rel="mask-icon" href={maskIcon} color="#5bbad5" />
+	<meta name="msapplication-TileColor" content="#2d89ef" />
+	<meta name="theme-color" content="#ffffff" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
 	<link
 		rel="stylesheet"
